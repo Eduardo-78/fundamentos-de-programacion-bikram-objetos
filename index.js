cb44459abe8e7175b170f6ff105890ccb303f0f7
Eduardo-78 <145039634+Eduardo-78@users.noreply.github.com>
2023-09-27 21:29:34 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+//01
+
+const Coche = {marca:"Honda", modelo:"HRV", matricula:"AB1234"};
+
+//02
+const Casa = {codPosta:"45621", calle:"23A", portal:"Alto", piso:"2"};
+
+//03
+const FullStackDeveloper ={lenguajes["JS"], proyectos["objeto"]};
+
+//04
+const Perro = {nombre:"", raza:"", color:"", edad:"",
+ladrar:function(){console.log("ladrido");},
+ popofun:function(){return Math.random()*3}}
+
+ //LECTURA PROPIEDADES
+//05
+let marcaPortatil = Portatil.marca;
+
+//06
+let marcaPortatil2 = Portatil["marca"];
+
+//07
+let grupos = Concierto.grupos;
+
+//08
+let RGB = [Led.Rojo,Led.Verde,Led.Azul];
+
+//MODIFICACION DE PROPIEDADES
+//09
+Portatil.modelo = "P345";
+
+//10
+Concierto.cartera.push("Guns N'Rose");
+
+//11
+Concierto.fecha = new Date(27/9/2023);
+
+//12
+Impresora.imprimiendo = {nombreArchivo: "", copias:"", numPaginas:"",};
+
+//DECLARACIONES
+//13
+const Noticia = {titular:"", cuerpo:"",}
+
+//14
+const Persona = {nombre:"", apellido:"", edad:"",}
+
+//15
+const Avion = {numPasajeros:"", despegar:function()
+    {console.log("despegando");}, volar:function()
+    {console.log("llegando al destino");},aterrizar:function()
+    {console.log("aterrizando");},}
+
+//16
+const Paquete = {contenido:[]};
+
+//17
+const Pais ={numHabitantes:"", continente:"", gentilicio:"",};
+
+//LECTURA PROPIEDADES
+//18
+let codError = O_Error.codigo;
+
+//19
+let integrantes = Grupo[integrantes];
+
+//20
+let nivelesTinta = Impresora.tinta;
+
+//21
+let pixeles = Pantalla.pixeles;
+
+//22
+let especificaciones = Movil.["especificaciones"];
+
+//MODIFICACION DE PROPIEDADES
+//23
+Grupo.numIntegrantes=5;
+
+//24
+Pantalla.dimensiones["1920 x 1080"];
+
+//25
+Led.encendido = !Boolean;
+
+//26
+Mobil.temperatura="20º";
